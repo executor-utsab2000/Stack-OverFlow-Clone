e7ee1c1de147b5bf35118f2615716d09e566ea2b
utsab <utsabsarkar00@gmail.com>
2024-06-15 12:19:37 +0530

    sdaddasf

--- Level 4 login system impliment/SignUp_LoginForm/login.php
+++ Level 4 login system impliment/SignUp_LoginForm/login.php
@@ -6,7 +6,7 @@
         </div>
         <div class="inputContainer mb-4">
             <label for="email">Enter Your Password : </label>
-            <input type="text" name="   " placeholder="Enter Your Email">
+            <input type="text" name="password" placeholder="Enter Your Email">
         </div>
         <div class="inputContainer mb-4">
             <input type="submit" value="Submit">
