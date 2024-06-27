2813c4143a4f5c141896ee387c6963168d4ecc1b
utsab <utsabsarkar00@gmail.com>
2024-06-27 23:51:38 +0530

    ssss

--- Level 7 user Profile/Admin Panel/index.php
+++ Level 7 user Profile/Admin Panel/index.php
@@ -13,3 +13,48 @@ require '../Frontend Components/cdnLinks.php';
 require '../Frontend Components/backendMsg.php';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.scss" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-fluid adminPanel px-0">
+        <!-- -------------------------------------------------- -->
+        <div class="navBar">
+            <img src="../Images/UI/Logo.png" alt="" />
+            <span class="ps-3">Admin Panel</span>
+
+            <div class="notificationButton">
+                <i class="fa-solid fa-bell"></i>
+            </div>
+            <div class="notificationBox ">
+                <div class="header"></div>
+                <div class="notifiicationContent">
+
+                    <div class="notificationTab">
+                        <div class="dp">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxIMA5q-wOA7Xegu1rx-BSdG5tmNLYIuCNvjMJ1JFHAc13zxlj2Fb0mB6MA7vGWAXrWZA&usqp=CAU"
+                                alt="" class="img-fluid">
+                        </div>
+                        <div class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus
+                            accusamus autem doloremque! Aspernatur, est eveniet.</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+        <!-- -------------------------------------------------- -->
+    </div>
+</body>
+<script type="module" src="../Script/Url Change BackendMsg/noParameterGetMsg.changeUrl.js"></script>
+
+</html>
